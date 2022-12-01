fbb815246406e78a1c873d060c416fc69b7bd8b8
Laura Pircalaboiu <laura.pirca@gmail.com>
2022-12-01 10:35:02 +0100

    make template

---
+++ src/_template/mod.rs
@@ -0,0 +1,13 @@
+fn part_1(file: &str) {
+    let contents = fs::read_to_string(file).expect("Should have been able to read the file");
+}
+
+
+fn part_2(file: &str) {
+    let contents = fs::read_to_string(file).expect("Should have been able to read the file");
+}
+
+pub fn run_day(file: &str){
+    part_1(file);
+    part_2(file);
+}
